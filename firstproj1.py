1629b44b579661d2848fbe737b7fddfb1e1c9c8f
bhavinv2 <114195826+bhavinv2@users.noreply.github.com>
2024-05-20 00:21:25 -0400

    updated code using definitions firstproj1.py

--- firstproj1.py
+++ firstproj1.py
@@ -1,56 +1,88 @@
-# Add Tasks: Users should be able to add new tasks to the list.
-# View Tasks: Users should be able to see all the tasks listed.
-# Edit Tasks: Users should be able to modify the details of existing tasks.
-# Delete Tasks: Users should be able to remove tasks from the list.
-
 def banner():
-    print ("Task manager")
+    print("Task Manager")
     print("Press 1 to add tasks")
     print("Press 2 to view tasks")
     print("Press 3 to edit tasks")
     print("Press 4 to delete tasks")
     print("Press 5 to end program")
 
+def add_task(task_list):
+    task = input("Enter a task: ")
+    task_list.append(task)
+    with open('data.txt', 'a') as file:
+        file.write(task + '\n')
+    print("Task added.")
 
-list_tasks=[]
+def view_tasks(task_list):
+    if task_list:
+        print("Your tasks:")
+        for idx, task in enumerate(task_list, start=1):
+            print(f"{idx}: {task}")
+    else:
+        print("No tasks to view.")
+
+def edit_task(task_list):
+    if task_list:
+        view_tasks(task_list)
+        try:
+            task_number = int(input("Enter the number of the task to edit: "))
+            if 1 <= task_number <= len(task_list):
+                new_task = input("Enter new task to replace with: ")
+                task_list[task_number - 1] = new_task
+                with open('data.txt', 'w') as file:
+                    for task in task_list:
+                        file.write(task + '\n')
+                print("Task edited")
+            else:
+                print("Invalid task number.")
+        except ValueError:
+            print("Please enter a valid number.")
+    else:
+        print("No tasks to edit.")
+
+def delete_task(task_list):
+    if task_list:
+        view_tasks(task_list)
+        try:
+            task_number = int(input("Enter the number of the task to delete: "))
+            if 1 <= task_number <= len(task_list):
+                task_list.pop(task_number - 1)
+                with open('data.txt', 'w') as file:
+                    for task in task_list:
+                        file.write(task + '\n')
+                print("Task deleted")
+            else:
+                print("Invalid task number.")
+        except ValueError:
+            print("Please enter a valid number.")
+    else:
+        print("No tasks to delete.")
 
 def controls():
+    task_list = []
+    try:
+        with open('data.txt', 'r') as file:
+            task_list = file.read().splitlines()
+    except FileNotFoundError:
+        pass
+
     while True:
         banner()
-        user_input=int(input("enter a number:"))
-        if user_input == 1:
-            add_task = input("Enter a task:")
-            add_task=str(add_task)
-            list_tasks.append(add_task)
-
-
-        elif user_input==2:
-            print("Your tasks:")
-            print(list_tasks)
-
-        elif user_input == 3:
-            print(list_tasks)
-            edit_task=int(input("select the index number of which task you'd like to edit:"))
-            print("You chose task:", list_tasks[edit_task])
-            new_edit_task= input("Enter the task you want to replace this with:")
-            list_tasks.remove(list_tasks[edit_task])
-            list_tasks.insert(edit_task,new_edit_task)
-
-
-        elif user_input == 4:
-            print(list_tasks)
-            delete_task=int(input("select the index number of which task you'd like to delete:"))
-            list_tasks.remove(list_tasks[delete_task])
-
-
-        elif user_input ==5:
-            break
-
-#test
+        try:
+            user_input = int(input("Choose a prompt: "))
+            if user_input == 1:
+                add_task(task_list)
+            elif user_input == 2:
+                view_tasks(task_list)
+            elif user_input == 3:
+                edit_task(task_list)
+            elif user_input == 4:
+                delete_task(task_list)
+            elif user_input == 5:
+                break
+            else:
+                print("Invalid option.")
+        except ValueError:
+            print("Please enter a valid number.")
 
 controls()
-
-
-
-
-
