8c7c872b09478b56b8887df8ecd21f88c5fac96d
bhavinv2 <114195826+bhavinv2@users.noreply.github.com>
2024-05-20 00:49:05 -0400

    advanced link list firstproj1.py

--- firstproj1.py
+++ firstproj1.py
@@ -1,88 +1,48 @@
-def banner():
-    print("Task Manager")
-    print("Press 1 to add tasks")
-    print("Press 2 to view tasks")
-    print("Press 3 to edit tasks")
-    print("Press 4 to delete tasks")
-    print("Press 5 to end program")
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
 
-def add_task(task_list):
-    task = input("Enter a task: ")
-    task_list.append(task)
-    with open('data.txt', 'a') as file:
-        file.write(task + '\n')
-    print("Task added.")
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
-def view_tasks(task_list):
-    if task_list:
-        print("Your tasks:")
-        for idx, task in enumerate(task_list, start=1):
-            print(f"{idx}: {task}")
-    else:
-        print("No tasks to view.")
+    def add(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
 
-def edit_task(task_list):
-    if task_list:
-        view_tasks(task_list)
-        try:
-            task_number = int(input("Enter the number of the task to edit: "))
-            if 1 <= task_number <= len(task_list):
-                new_task = input("Enter new task to replace with: ")
-                task_list[task_number - 1] = new_task
-                with open('data.txt', 'w') as file:
-                    for task in task_list:
-                        file.write(task + '\n')
-                print("Task edited")
-            else:
-                print("Invalid task number.")
-        except ValueError:
-            print("Please enter a valid number.")
-    else:
-        print("No tasks to edit.")
+    def delete(self, index):
+        if not self.head:
+            return
+        if index == 0:
+            self.head = self.head.next
+            return
+        current = self.head
+        for i in range(index - 1):
+            if not current.next:
+                return
+            current = current.next
+        if current.next:
+            current.next = current.next.next
 
-def delete_task(task_list):
-    if task_list:
-        view_tasks(task_list)
-        try:
-            task_number = int(input("Enter the number of the task to delete: "))
-            if 1 <= task_number <= len(task_list):
-                task_list.pop(task_number - 1)
-                with open('data.txt', 'w') as file:
-                    for task in task_list:
-                        file.write(task + '\n')
-                print("Task deleted")
-            else:
-                print("Invalid task number.")
-        except ValueError:
-            print("Please enter a valid number.")
-    else:
-        print("No tasks to delete.")
+    def edit(self, index, new_data):
+        current = self.head
+        for i in range(index):
+            if not current.next:
+                return
+            current = current.next
+        current.data = new_data
 
-def controls():
-    task_list = []
-    try:
-        with open('data.txt', 'r') as file:
-            task_list = file.read().splitlines()
-    except FileNotFoundError:
-        pass
-
-    while True:
-        banner()
-        try:
-            user_input = int(input("Choose a prompt: "))
-            if user_input == 1:
-                add_task(task_list)
-            elif user_input == 2:
-                view_tasks(task_list)
-            elif user_input == 3:
-                edit_task(task_list)
-            elif user_input == 4:
-                delete_task(task_list)
-            elif user_input == 5:
-                break
-            else:
-                print("Invalid option.")
-        except ValueError:
-            print("Please enter a valid number.")
-
-controls()
+    def traverse(self):
+        tasks = []
+        current = self.head
+        while current:
+            tasks.append(current.data)
+            current = current.next
+        return tasks
